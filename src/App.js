955aa2490603e54cc8a3c5ca82f7976390cf502e
Kamel BENDIMERAD <kamel.bendimerad@gmail.com>
2022-07-17 14:15:30 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="result-equation">
+      <p id="equation">hey</p>
+      <p id="result">hello</p>
+      </div>
+      <div className="grid-container">
+      <button id="ac" className="span-two">AC</button>
+      <button id="del"  >DEL</button>
+      <button id="devide">/</button>
+      <button id="one">1</button>
+      <button id="two">2</button>
+      <button id="three">3</button>
+      <button id="multiply">*</button>
+      <button id="four">4</button>
+      <button id="five">5</button>
+      <button id="six">6</button>
+      <button id="plus">+</button>
+      <button id="seven">7</button>
+      <button id="eight">8</button>
+      <button id="nine">9</button>
+      <button id="minus">-</button>
+      <button id="dot">.</button>
+      <button id="zero">0</button>      
+      <button className="span-two" id="equals">=</button>
+      </div>
+
+
+
     </div>
   );
 }
